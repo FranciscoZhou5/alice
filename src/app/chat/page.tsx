e1874bcab7210e782049c0088601266883dd9ab9
FranciscoZhou5 <fg45874587@proton.me>
2023-06-06 21:28:38 -0300

    fix: use chat without username or email

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,7 +2,16 @@ import ChatMessage from "@/components/ChatMessages";
 import PromptInput from "@/components/PromptInput";
 import Hero from "@/components/Hero";
 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 export default function ChatPage() {
+  const cookiesStore = cookies();
+
+  if (!cookiesStore.has("username") && !cookiesStore.has("email")) {
+    redirect("/");
+  }
+
   return (
     <main>
       <div>
